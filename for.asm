9c676dcb793d259b95f1d61f3d6b340dac018044
SkiaBr23 <rafaelcosta.rdc@hotmail.com>
2017-05-07 22:24:06 -0300

    Corrigindo cagada de conflito do git no código

--- for.asm
+++ for.asm
@@ -1,8 +1,18 @@
+; Grupo 7
+; 
+; Rafael Dias da Costa 12/0133253
+;
+; Macro "FOR" em NASM.
+;
+; Arquivo para teste em C: for.c
+;
+; Comando para gerar objeto: nasm -f elf -d ELF_TYPE for.asm   (Para Linux, desenvolvido em Ubuntu 16.04)
+;
 
-%include "asm_io.inc"
 
-; Macro de início da iteração
-%macro for 1
+%include "asm_io.inc"		; inclusão da biblioteca de I/O do Carter 
+
+%macro for 1 				; Macro de início da iteração
 	%push for    			; executa o push da macro
 	j%-1 %$endloop  		; como argumento eh nz, monta 'jnz endloop'
 	%$startloop:  			; inicia corpo do for
@@ -11,15 +21,9 @@
 
 ; Macro de verificação de fim de interação
 %macro endfor 0
-<<<<<<< HEAD
-	jnz %$startloop  ; verifica se deve continuar iteracoes
-	%$endloop:       ; se nao, termina a iteracao
-	%pop             ; remove da pilha
-=======
 	jnz %$startloop  		; verifica se deve continuar iteracoes
 	%$endloop:       		; se nao, termina a iteracao
 	%pop             		; remove da pilha 
->>>>>>> 1022f932949b01ccc478c90c213546ee7481bd9b
 %endmacro
 
 global forSB	 			; declaracao do nome da macro na interface C
@@ -33,20 +37,13 @@ total_msg 		db " iteracoes executadas no total", 0xa, 0
 	section .text
 
 	forSB:
-<<<<<<< HEAD
-	mov eax,[esp+4]   ; captura do argumento 1
-	mov ebx,[esp+8]   ; captura do argumento 2 (Numero de iterações desejada)
-	mov ecx,[esp+12]  ; captura do argumento 3 (Passo da iteração, Diferente de 0 = passo incremental, Igual a 0 = decremental)
 
-	cmp eax,ebx
-=======
 	mov eax, iteracao_msg   ; mensagem de iteracao
 	mov edx,[esp+4]   		; captura do argumento 1
 	mov ebx,[esp+8]   		; captura do argumento 2 (Numero de iterações desejada)
 	mov ecx,[esp+12]  		; captura do argumento 3 (Passo da iteração, Diferente de 0 = passo incremental, Igual a 0 = decremental)
 	mov edi, 0
 	cmp edx,ebx       
->>>>>>> 1022f932949b01ccc478c90c213546ee7481bd9b
 
 	for nz            		; chama a macro 'for' declarada na linha 10
 		cmp ecx,0     		; compara argumento de passo com 0
@@ -64,14 +61,9 @@ total_msg 		db " iteracoes executadas no total", 0xa, 0
 		inc edi            	; incrementa o registrador contador de iteracoes
 		cmp edx,ebx   		; condicao de parada
 
-<<<<<<< HEAD
-	endfor            ; chama a macro 'endfor' declarada na linha 18
-	ret               ; retorna ao programa C
-=======
 	endfor            		; chama a macro 'endfor' declarada na linha 18
 	mov eax, edi
 	call print_int    		; imprime na tela o total de iteracoes
 	mov eax, total_msg  	; imprime na tela a cada iteracao
 	call print_string
 	ret               		; retorna ao programa C
->>>>>>> 1022f932949b01ccc478c90c213546ee7481bd9b
